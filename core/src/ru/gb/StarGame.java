7c50489f4e8eec853155654236fa79b0cbe9af8e
MPI333 <mpi333@inbox.ru>
2021-08-29 15:07:37 +0300

    origin

--- core/src/ru/gb/StarGame.java
+++ core/src/ru/gb/StarGame.java
@@ -6,26 +6,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture background;
+	SpriteBatch batch;
+	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		background = new Texture("space_background.png");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0f, 0f, 0f, 1f);
+		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(background, 0, 0);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		background.dispose();
+		img.dispose();
 	}
 }
